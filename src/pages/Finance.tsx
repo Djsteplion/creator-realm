f1ce5ba3f3a373e58c97bb6ef45d55718d504a12
Stephen Olayiwola <djsteplion@gmail.com>
2026-04-02 00:42:51 +0100

    Refactor Finance component and enhance functionality

--- src/pages/Finance.tsx
+++ src/pages/Finance.tsx
@@ -9,7 +9,6 @@ import { useQuery } from '@tanstack/react-query';
 import {useState} from 'react';
 import axios from 'axios';
 import type { ApiTransactionsResponse } from '../types/finance';
-import type { ApiDashboardResponse } from '../types/dashboard'; // Added this import
 import LoadingScreen  from '../components/LoadingScreen';
 
 // Define a local interface for your table rows if you haven't already
