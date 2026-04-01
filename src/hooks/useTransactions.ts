d0e5106d8032a7ddf05f57782b7aa7af729c6816
Stephen Olayiwola <djsteplion@gmail.com>
2026-04-01 23:47:21 +0100

    Update useTransactions.ts

--- src/hooks/useTransactions.ts
+++ src/hooks/useTransactions.ts
@@ -1,14 +1,1 @@
 
-import { useQuery } from '@tanstack/react-query';
-import api from '../api/axiosInstance';
-
-export const useTransactions = (status?: string) => {
-  return useQuery({
-    queryKey: ['transactions', status],
-    queryFn: async () => {
-      const params = status && status !== 'all' ? { status } : {};
-      const { data } = await api.get('/transactions', { params });
-      return data.data;
-    },
-  });
-};
